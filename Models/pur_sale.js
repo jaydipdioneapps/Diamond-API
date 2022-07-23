d9012e5c88ce56cc94d8a0c84bd335c1f133dc62
vinusmavani <91609777+jaydipdioneapps@users.noreply.github.com>
2022-07-23 13:13:25 +0530

    two table merging

--- Models/pur_sale.js
+++ Models/pur_sale.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const pur_saleSchema = mongoose.Schema({
-    inv_type: { type: Boolean, required: true },
-    date: { type: Date, default: new Date().toISOString().slice(0, 10) },
+    inv_type: { type: Boolean, default: false },
+    date: { type: Date, default: Date.now },
     type: { type: String, default: "" },
-    inv_no: { type: Number, required: true, unique: true },
+    inv_no: { type: Number, unique: true },
     bill_no: { type: Number, default: "" },
     party: { type: String, default: "" },
     due_days: { type: String, default: "" },
@@ -43,6 +43,34 @@ const pur_saleSchema = mongoose.Schema({
     status: { type: String, default: "" },
     branch_id: { type: String, default: "" },
     master_country_id: { type: String, default: "" },
+    trans_entries: [
+        {
+            date: { type: Date, default: "" },
+            refno: { type: mongoose.Schema.Types.ObjectId, ref: 'Item_master' },
+            lotno: { type: String, default: "" },
+            pcs: { type: Number, default: "" },
+            carat: { type: Number, default: "" },
+            rate: { type: Number, default: "" },
+            amount: { type: Number, default: "" },
+            l1: { type: Number, default: "" },
+            l2: { type: Number, default: "" },
+            net_amount: { type: Number, default: "" },
+            kapan: { type: String, default: "" },
+            remark: { type: String, default: "" },
+            asking_rate: { type: Number, default: "" },
+            cost: { type: String, default: "" },
+            stock_value: { type: String, default: "" },
+            status: { type: String, default: "" },
+            c_u_id: { type: String, default: "" },
+            current_date: { type: Date, default: "" },
+            current_time: { type: Date, default: "" },
+            u_u_id: { type: String, default: "" },
+            u_date: { type: Date, default: "" },
+            u_time: { type: Date, default: "" },
+            branch_id: { type: String, default: "" },
+            master_country_id: { type: String, default: "" },
+        }
+    ]
 });
 
 module.exports = mongoose.model("Pur_sale", pur_saleSchema);
