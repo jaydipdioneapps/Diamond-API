0024c46da2de8208a901a2902fcada6c4e4b7650
dioneapps <jaydip@dioneapps.com>
2022-08-16 16:04:32 +0530

    change in carat counter

--- Controllers/caratController.js
+++ Controllers/caratController.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const caratcounter = require("../Models/caratcounter");
 const item_master = require("../Models/item_master");
 
@@ -8,12 +7,17 @@ exports.get_carat = async function (req, res, next) {
         let newdata = await caratcounter.find();
         for (let i = 0; i < newdata.length; i++) {
             let lotdata = await item_master.findOne({ refno: newdata[i].refno }, {
-                lotno: 1
+                // lotno: 1,
+                // pcs: 1
             });
+            // console.log(lotdata);
             let newObj = {
                 refno: newdata[i].refno,
                 lotno: lotdata.lotno,
+                kapan: lotdata.kapan,
+                rate: lotdata.asking_rate,
                 total_carat: newdata[i].purchase + newdata[i].memo_in - newdata[i].sale - newdata[i].memo_out,
+                pcs: newdata[i].pcs,
             }
             newArry.push(newObj);
         }
